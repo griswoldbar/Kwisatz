27e465ea4be8c1b045ac7ba1b7b9a92bd3075960
Christopher Turtle <hosmerangel@gmail.com>
2011-02-03 21:53:29 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kwisatz::Application.config.secret_token = 'd4ec5290c858e24eefd2de23dc7ff3f9d361fe6f517a06b7ee58a9d5ef589607e0596cc33e5195dff891fd60c43a44fcdd47026769451b7ec93d211fdc2f833f'
