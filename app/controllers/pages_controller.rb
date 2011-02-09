c44ecc87e123b69a7e7a08c6037131b77230a1fa
Christopher Turtle <hosmerangel@gmail.com>
2011-02-09 16:44:53 +0000

    User sign in and sign out are there, as are layout links

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,18 @@
 class PagesController < ApplicationController
   def home
-    @title=Kwisatz
+    @title="Kwisatz"
   end
 
   def contact
+    @title = "Contact"
+  end
+
+  def about
+    @title = "About"
+  end
+
+  def help
+    @title = "Help"
   end
 
 end
